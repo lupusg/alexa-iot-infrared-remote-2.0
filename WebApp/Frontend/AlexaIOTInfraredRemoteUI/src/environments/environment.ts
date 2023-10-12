a841e930b175d182f238916d6b0302273381ebc9
Vlad Lupu <lupusvlad@gmail.com>
2023-10-12 11:58:32 +0000

    Merged PR 30: Fix env urls for production
    \

--- WebApp/Frontend/AlexaIOTInfraredRemoteUI/src/environments/environment.ts
+++ WebApp/Frontend/AlexaIOTInfraredRemoteUI/src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    authServerUrl: 'https://aiir-web2.azurewebsites.net/',
+    apiUrl: 'https://aiir-web2.azurewebsites.net/resource-server/api'
+  }
+  
