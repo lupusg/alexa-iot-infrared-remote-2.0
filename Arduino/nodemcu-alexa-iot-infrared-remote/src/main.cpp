68cba8c81fa759ec7f493986abdd93eb693660aa
Vlad Lupu <lupusvlad@gmail.com>
2023-10-30 12:15:42 +0000

    Merged PR 52: Configure ESP32 for HTTPS Requests
    Related work items: #74

--- Arduino/nodemcu-alexa-iot-infrared-remote/src/main.cpp
+++ Arduino/nodemcu-alexa-iot-infrared-remote/src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "arduino-iot-cloud-connection.h"
 #include "config.h"
+#include "http-client-secure.h"
 #include "infrared-receiver.h"
 
 InfraredReceiver infrared_receiver(RECV_PIN, CAPTURE_BUFFER_SIZE,
@@ -9,13 +10,17 @@ InfraredReceiver infrared_receiver(RECV_PIN, CAPTURE_BUFFER_SIZE,
 ArduinoIoTCloudConnection iot_cloud_connection(WIFI_SSID, WIFI_PASSWORD,
                                                DEVICE_LOGIN_NAME, DEVICE_KEY);
 
+HTTPClientSecure http_client_secure(WIFI_SSID, WIFI_PASSWORD, API_CERTS);
+
 void setup() {
   Serial.begin(BAUD_RATE);
   iot_cloud_connection.setup();
   infrared_receiver.setup();
+  http_client_secure.setup();
 }
 
 void loop() {
   iot_cloud_connection.loop();
   infrared_receiver.loop();
+  http_client_secure.loop();
 }
