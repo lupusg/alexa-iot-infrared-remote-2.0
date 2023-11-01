8929731c8c9f7370f84c5a41a7f59667cdcd8f98
Alexandra Pirvuletu <pirvuletu.e.alexandra@gmail.com>
2023-11-01 16:12:20 +0000

    Merged PR 54: Create specification class
    Create BaseSpecification class and ISpecification interface
    
    Related work items: #79

---
+++ WebApp/Backend/AlexaIOTInfraredRemoteAPI/AlexaIOTInfraredRemoteAPI.Domain/Specifications/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace AlexaIOTInfraredRemoteAPI.Domain.Specifications
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
